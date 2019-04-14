4d5cc55b341cd603c0aa0ca3f832e4a566ebe759
Amad-Mlinka <49623976+Amad-Mlinka@users.noreply.github.com>
2019-04-15 01:32:39 +0200

    Add files via upload

---
+++ Quiz game/script.js
@@ -0,0 +1,387 @@
+var app = angular.module('myApp', []);
+
+function myController($scope) {
+  $scope.hide_intro=false;
+  /*****************Write your own anwsers pitanja****************/
+  $scope.pitanja_Write_Y = {
+    /*****************Nivo 1*******************/
+    "nivo1": [{
+      "red": 1,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 2,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+
+      "red": 3,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+
+    }, {
+      "red": 4,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 5,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 6,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }],
+    /*****************Nivo 2*******************/
+    "nivo2": [{
+      "red": 1,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 2,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+
+      "red": 3,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+
+    }, {
+      "red": 4,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 5,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 6,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }],
+    /*****************Nivo 3*******************/
+    "nivo3": [{
+      "red": 1,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 2,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+
+      "red": 3,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+
+    }, {
+      "red": 4,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 5,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 6,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }]
+  }
+  /*****************Multiple choice pitanja****************/
+  $scope.pitanja_Write_Y = {
+    /*****************Nivo 1*******************/
+    "nivo1": [{
+      "red": 1,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 2,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+
+      "red": 3,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+
+    }, {
+      "red": 4,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 5,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 6,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }],
+    /*****************Nivo 2*******************/
+    "nivo2": [{
+      "red": 1,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 2,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+
+      "red": 3,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+
+    }, {
+      "red": 4,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 5,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 6,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }],
+    /*****************Nivo 3*******************/
+    "nivo3": [{
+      "red": 1,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 2,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+
+      "red": 3,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+
+    }, {
+      "red": 4,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 5,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 6,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }]
+  }
+  /*****************Multiple choice pitanja****************/
+  $scope.pitanja_true_False = {
+    /*****************Nivo 1*******************/
+    "nivo1": [{
+      "red": 1,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 2,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+
+      "red": 3,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+
+    }, {
+      "red": 4,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 5,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 6,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }],
+    /*****************Nivo 2*******************/
+    "nivo2": [{
+      "red": 1,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 2,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+
+      "red": 3,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+
+    }, {
+      "red": 4,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 5,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 6,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }],
+    /*****************Nivo 3*******************/
+    "nivo3": [{
+      "red": 1,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 2,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+
+      "red": 3,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+
+    }, {
+      "red": 4,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 5,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }, {
+      "red": 6,
+      "pitanje": "LG Nexus 5",
+      "odgovor": "LG",
+      "broj bodova": "LG"
+
+    }]
+
+
+
+  }
+  
+   $scope.slanje = function() {
+    if ($scope.odabir_Vrste) {
+      $scope.hide_intro = true;
+    }
+  }
+}
+
+myController.$inject = ['$scope'];
+
+app.controller('myController', myController);
